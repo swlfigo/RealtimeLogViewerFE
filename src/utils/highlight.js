511b525ff98dcb5ebb3fe1d1443c12f8ce7d7481
sylar <swlfigo@gmail.com>
2025-06-23 14:28:23 +0800

    Fix escapeHtml Problem

--- src/utils/highlight.js
+++ src/utils/highlight.js
@@ -1,6 +1,18 @@
+function escapeHtml(text) {
+  return text.replace(/[&<>"']/g, function (c) {
+    return {
+      '&': '&amp;',
+      '<': '&lt;',
+      '>': '&gt;',
+      '"': '&quot;',
+      "'": '&#39;'
+    }[c];
+  });
+}
+
 export function highlightText(text, searchText = '') {
-  if (!searchText) return text
-  
-  const regex = new RegExp(`(${searchText})`, 'gi')
-  return text.replace(regex, '<span class="highlight">$1</span>')
+  const safeText = escapeHtml(text)
+  if (!searchText) return safeText
+  const regex = new RegExp(`(${searchText.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')})`, 'gi')
+  return safeText.replace(regex, '<span class="highlight">$1</span>')
 } 
